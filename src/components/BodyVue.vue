d542ba0b92dba446ebf61ef915f4417a84a048cf
20040s0035 <103164385+20040s0035@users.noreply.github.com>
2022-12-14 17:00:16 +0900

    BodyVue.vue
    0始まりの数字が入力できる件を解消した。

--- src/components/BodyVue.vue
+++ src/components/BodyVue.vue
@@ -19,7 +19,7 @@ export default {
         return {
             el: '#calculate',
             formula: "0",
-            number: 0,
+            period_included: false,
             dataArrays: [
                 [7, 8, 9, "÷"],
                 [4, 5, 6, "×"],
@@ -33,50 +33,54 @@ export default {
         selectedNumber: function ($event, val) {
             //1. "=" の時
             if (val == "=") {
+                this.period_included = false;
                 this.formula = this.calculate;
-            //2. "÷, ×, -, +" の時
+                //2. "÷, ×, -, +" の時
             } else if (this.operations.includes(val) == true) {
+                this.period_included = false;
                 if (this.operations.some(c => c === this.formula.slice(-1)) == true) {
                     this.formula = this.formula.slice(0, -1);
                     this.addContext($event);
-                } else if (this.formula.slice(-1) == "."){
+                } else if (this.formula.slice(-1) == ".") {
                     this.formula = this.formula.slice(0, -1);
                     this.addContext($event);
                 } else {
                     this.addContext($event);
                 }
-            //3. "." の時
-            }else if (val == "."){
+                //3. "." の時
+            } else if (val == ".") {
+                this.period_included = true;
                 if (this.operations.some(c => c === this.formula.slice(-1)) == true) {
                     this.formula += "0";
                     this.addContext($event);
-                } else if (this.formula.slice(-1) == "."){
-                    this.formula = this.formula.slice(0, -1);
-                    this.addContext($event);
+                } else if (this.formula.slice(-1) == ".") {
+                    return;
                 } else {
                     this.addContext($event);
                 }
-            //4. "0" の時
-            }else if (val == "0"){
-            //5. 1,2,3,4,5,6,7,8,9 の時
+                //4. "0" の時
+            } else if (val == "0") {
+                if (this.period_included == false && this.formula.slice(-1) == "0") {
+                    return;
+                } else {
+                    this.addContext($event);
+                }
+                //5. "1,2,3,4,5,6,7,8,9" の時
             } else {
                 if (this.formula == "0") {
                     this.formula = "";
-                } else if (this.formula.slice(-1) == 0 && this.formula.slice(-2, -1).isNaN() == true) {
+                } else if (this.period_included == false && this.formula.slice(-1) == "0") {
                     this.formula = this.formula.slice(0, -1);
                 }
                 this.addContext($event);
             }
+
             console.log(this.formula);
-        },
-        extractNumber: function() {
-            formula.indexOf(this.operations)
-            this.number = this.formula;
         },
         addContext: function ($event) {
             this.formula += $event.target.textContent;
         },
-        
+
         clearAll: function () {
             this.formula = "0";
         }
